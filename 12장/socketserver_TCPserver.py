dcfc9b93aaceb3c4b758d7f97f83aec245ff1bbe
yeolkyu <tenque@naver.com>
2022-01-14 23:08:31 +0900

    modified

--- 12장/socketserver_TCPserver.py
+++ 12장/socketserver_TCPserver.py
@@ -2,35 +2,18 @@
 import socketserver
 
 class TCPHandler(socketserver.BaseRequestHandler):
-    def setup(self): #연결 설정
-        print("Connected")
-        
     def handle(self): #데이터 수신
-        while True: #연결을 유지하려면 무한 루프 실행
-            # self.request = client socket
-            try:
-                self.data = self.request.recv(1024).strip()
-                if not self.data:
-                    #self.finish()
-                    return
-            except:
-                self.finish()
-                return
-            print(("{} wrote:".format(self.client_address[0])))
-            print((self.data))
+        self.data = self.request.recv(1024).strip()
+        print(("{} wrote:".format(self.client_address[0])))
+        print((self.data))
             
-
-            # 수신 메시지 전송
-            self.request.sendall(self.data.upper())
-    
-    def finish(self): #연결 해제
-        print("Disconnected")
-        self.request.close()
+        # 수신 메시지 전송
+        self.request.sendall(self.data.upper())
             
 if __name__ == "__main__":
     HOST, PORT = "localhost", 2500
     # 서버 객체 생성
     server = socketserver.TCPServer((HOST, PORT), TCPHandler, bind_and_activate=True)
 
-    # 서버 실행
+    # 연결 대기 동작 계속
     server.serve_forever()
