39f1eba1b54afbcfa7a4da8c83d87cea4821b3ea
yeolkyu <tenque@naver.com>
2022-05-10 22:30:32 +0900

    2022-5-9

--- 11장/errorcontrol_UDP_client.py
+++ 11장/errorcontrol_UDP_client.py
@@ -11,24 +11,26 @@ c_sock.connect((server, port))
 total_delay = 0 # total delay time for 10 trials
 
 for i in range(10): #10번 시도
-    delay = 0.1  #0.1초부터 지연 시작
+    delay = 0.1  #0.1초부터 대기 시작
     sub_total = 0 # total delay time for one trial
-    data = 'Hello message'
+    data = 'Hello message ' + str(i)
 
     while True:
+        print("\nSending:", data)
         c_sock.send(data.encode())
         print(f'Waiting up to {delay} seconds for a reply')
         c_sock.settimeout(delay) #타임아웃 설정
         try:
-            data = c_sock.recv(BUFFER) #데이터 수신
+            resp = c_sock.recv(BUFFER) #데이터 수신
         except timeout: #타임아웃 발생
+            print("No response!!!")
             delay *= 2  #대기 시간 2배 증가
             if delay > 2.0: #시간 초과
                 print('The server seems to be down')
                 break 
         else:
             sub_total += delay 
-            print(data.decode(), "received")
+            print(resp.decode(), "received")
             break #종료
         
     total_delay += sub_total
--- 11장/errorcontrol_UDP_server.py
+++ 11장/errorcontrol_UDP_server.py
@@ -5,14 +5,16 @@ from socket import *
 
 port = 2500
 BUFFER = 1024
+ErrorRate = 5
+
 s_sock = socket(AF_INET, SOCK_DGRAM)
 s_sock.bind(('', port))
 print('Waiting for data')
 
 while True:
     data, address = s_sock.recvfrom(BUFFER)
-    if random.randint(1, 10) < 4: #패킷 손실
+    if random.randint(1, 10) < ErrorRate: #패킷 손실
         print(f'Bad! Packet from {address} lost!!!')
     else:
-        print(f'Good! Message is {data.decode()!r} from {address}') #메시지 출력
+        print(f'Good! Message is {data.decode()} from {address}') #메시지 출력
         s_sock.sendto('ACK'.encode(), address) #ACK 응답 전송
