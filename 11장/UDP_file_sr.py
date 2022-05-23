c6fe6a019b2639009463b6b4ac81f65cf7d28eb4
yeolkyu <tenque@naver.com>
2022-05-23 14:14:42 +0900

    2022-5-23

--- 11장/UDP_file_sr.py
+++ 11장/UDP_file_sr.py
@@ -1,24 +1,29 @@
 #UDP 소켓을 이용한 파일 송수신 프로그램
 #수신을 먼저 실행하고 송신한다
 
-import socket, time, sys
+import socket, time, sys, os
 
-BUFSIZE = 1024*8
+BUFSIZE = 8096*8
 
 def Receiver():
     sock.bind(addr)
     
     print("수신 대기 중...")
-    file_name, client = sock.recvfrom(BUFSIZE) #파일 이름 수신
+    fn, client = sock.recvfrom(BUFSIZE) #파일 이름 수신
+    fn = os.path.basename(fn.decode()) # 기본 파일이름 추출
+    print("파일 이름: ", fn)
     sock.sendto('ok'.encode(), client) #응답
     
-    with open('d:/'+file_name.decode(), "wb") as fp:
+    path = 'c:/temp/' # 파일 저장 경로
+    with open(path+fn, "wb") as fp:
         print("파일 수신 중...")
         while True:
-            data = sock.recvfrom(BUFSIZE)
-            if not data[0]: #수신 완료
+            data, _ = sock.recvfrom(BUFSIZE)
+            if not data: #수신 완료
                 break
-            fp.write(data[0]) #수신 데이터를 파일에 저장
+            fp.write(data) #수신 데이터를 파일에 저장
+            
+    print("Saved in "+ path + fn)
     
     print("수신 완료")
 
@@ -29,6 +34,7 @@ def Sender():
     
     if resp.decode().lower() == 'ok':
         fp = open(file_name, "rb")
+        
         data = fp.read(BUFSIZE)
         print("송신 중...")
     
@@ -38,6 +44,7 @@ def Sender():
             data = fp.read(BUFSIZE)
             
         sock.sendto(data, client) #송신 완료를 위해 빈 데이터 전송
+        
         fp.close()
         print("전송 완료")
     else:
@@ -55,4 +62,4 @@ if __name__ == "__main__":
         else:
             print("s(송신) 또는 r(수신)을 지정하세요")
     except:
-        print("s(송신) 또는 r(수신)을 지정하세요")
+        print("송수신 오류")
