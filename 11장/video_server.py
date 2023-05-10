5e5ba10401ae80352e4f12075400688499898f8c
yeolkyu <tenque@naver.com>
2023-05-10 15:05:43 +0900

    Cam or MP4

--- 11장/video_server.py
+++ 11장/video_server.py
@@ -20,20 +20,22 @@ print("서버 접속 대기:", server_addr)
 while True:
     client_socket, addr = server_socket.accept()
     print(addr, '와 연결됨')
+    mode = int(input("WebCam(1) or MP4(2): "))
     
     if client_socket:
-        # WebCAM 사용
-        vid = cv2.VideoCapture(0) # WebCam
-        
-        # 동영상 플레이
-        #vid = cv2.VideoCapture("동양미래대학교.mp4") # WebCam
-        #if vid.isOpened(): print('width: {}, height : {}'.format(vid.get(3), vid.get(4)))
+        if mode == 1:
+            # WebCAM 사용
+            vid = cv2.VideoCapture(0) # WebCam
+        else:
+            # 동영상 플레이
+            vid = cv2.VideoCapture("동양미래대학교.mp4") # MP4
+            if vid.isOpened(): print('width: {}, height : {}'.format(vid.get(3), vid.get(4)))
 
         while (vid.isOpened()):
             img, frame = vid.read() # 프레임 획득
             if not img:
                 break
-            frame = imutils.resize(frame, width=640) # 프레임 크기 조절
+            frame = imutils.resize(frame, width=1024) # 프레임 크기 조절
             frame_bytes = pickle.dumps(frame) # 프레임을 바이트 스트림으로 변환
             message = struct.pack("Q", len(frame_bytes)) + frame_bytes # frame 길이(unsigned 8bytes) + frame
             try:
